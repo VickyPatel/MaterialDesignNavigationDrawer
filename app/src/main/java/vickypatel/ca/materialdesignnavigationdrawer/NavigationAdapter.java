b3f392c7860404d06b9f36a814046796fb7163b2
Vicky <vicky.7676@yahoo.in>
2015-08-30 16:07:40 -0400

    more item added

--- app/src/main/java/vickypatel/ca/materialdesignnavigationdrawer/NavigationAdapter.java
+++ app/src/main/java/vickypatel/ca/materialdesignnavigationdrawer/NavigationAdapter.java
@@ -13,17 +13,20 @@ import android.widget.TextView;
 public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.ViewHolder> {
 
     private static final int TYPE_HEADER = 0;
-    private static final int TYPE_ITEM = 1;
+    private static final int TYPE_ITEM1 = 1;
+    private static final int TYPE_ITEM2 = 2;
 
     private String[] titles;
+    private String[] titles2;
     private int[] icons;
     private String name;
     private int profile;
     private String email;
 
 
-    public NavigationAdapter(String[] titles, int[] icons, String name, String email, int profile) {
+    public NavigationAdapter(String[] titles, String[] titles2, int[] icons, String name, String email, int profile) {
         this.titles = titles;
+        this.titles2 = titles2;
         this.icons = icons;
         this.profile = profile;
         this.name = name;
@@ -34,14 +37,17 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         ViewHolder viewHolder = null;
-        if (viewType == TYPE_ITEM) {
+        if (viewType == TYPE_ITEM1) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_row_navigation_drawer, parent, false);
             viewHolder = new ViewHolder(view, viewType);
-     
+
         } else if (viewType == TYPE_HEADER) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.header_navigation_drawer, parent, false);
             viewHolder = new ViewHolder(view, viewType);
 
+        } else if (viewType == TYPE_ITEM2) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_row_navigation_drawer, parent, false);
+            viewHolder = new ViewHolder(view, viewType);
         }
         return viewHolder;
     }
@@ -51,8 +57,11 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
         if (holder.Holderid == 1) {
             holder.textView.setText(titles[position - 1]);
             holder.imageView.setImageResource(icons[position - 1]);
-        }else{
-           holder.profile.setImageResource(profile);
+        } else if (holder.Holderid == 2) {
+            holder.textView2.setText(titles2[position - 5]);
+            holder.imageView2.setImageResource(icons[position - 5]);
+        } else if(holder.Holderid == 0){
+            holder.profile.setImageResource(profile);
             holder.name.setText(name);
             holder.email.setText(email);
         }
@@ -60,26 +69,30 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
 
     @Override
     public int getItemCount() {
-        return titles.length+1;
+        return 1 + titles.length  + titles2.length;
     }
 
     @Override
     public int getItemViewType(int position) {
-        if(isHeader(position)){
-            return  TYPE_HEADER;
-        }else {
-            return TYPE_ITEM;
+        int currentPosition = 0;
+        if (position == 0) {
+            currentPosition = TYPE_HEADER;
+        } else if (position >= 1 && position < 5) {
+            currentPosition = TYPE_ITEM1;
+        } else if(position >= 5){
+            currentPosition = TYPE_ITEM2;
         }
+        return  currentPosition;
     }
 
-    private boolean isHeader(int position){
-        return position == 0;
-    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         int Holderid;
         TextView textView;
         ImageView imageView;
+        TextView textView2;
+        ImageView imageView2;
         ImageView profile;
         TextView name;
         TextView email;
@@ -87,12 +100,15 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
         public ViewHolder(View itemView, int viewType) {
             super(itemView);
 
-            if (viewType == TYPE_ITEM) {
+            if (viewType == TYPE_ITEM1) {
                 textView = (TextView) itemView.findViewById(R.id.rowText);
                 imageView = (ImageView) itemView.findViewById(R.id.rowIcon);
                 Holderid = 1;
-
-            } else {
+            } else if (viewType == TYPE_ITEM2) {
+                textView2 = (TextView) itemView.findViewById(R.id.rowText);
+                imageView2 = (ImageView) itemView.findViewById(R.id.rowIcon);
+                Holderid = 2;
+            } else if(viewType == TYPE_HEADER) {
                 name = (TextView) itemView.findViewById(R.id.name);
                 email = (TextView) itemView.findViewById(R.id.email);
                 profile = (ImageView) itemView.findViewById(R.id.circleImageView);
